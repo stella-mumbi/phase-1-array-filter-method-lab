cad6a06d4300414320bb1fb2faeb7be32a82bc42
stella-mumbi <stella.mumbi@student.moringaschool.com>
2023-12-21 10:50:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+function fuzzyMatch(drivers, name){
+    return drivers.filter(driver => driver.toLowerCase().indexOf(name.toLowerCase()) === 0);
+}
+function matchName(drivers, homeTown){
+return drivers.filter((record) => record.name === homeTown);
+}
